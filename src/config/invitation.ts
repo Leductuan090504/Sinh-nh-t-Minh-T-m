553b42e68b59ab10ffc95f95e4fdcc4fdcb694c3
Tuấn đen <tuan10c7ngt@gmail.com>
2026-06-15 05:54:18 +0700

    fix times

--- src/config/invitation.ts
+++ src/config/invitation.ts
@@ -3,7 +3,7 @@ export const invitation = {
   birthdayName: 'Huỳnh Thị Minh Tâm',
   date: '2026-06-16',
   displayDate: '16/06/2026',
-  time: '19:00',
+  time: '19:30',
   locationName: 'Ốc Đêm Chú Kiệt',
   mapsUrl:
     'https://www.google.com/maps/place/%E1%BB%90c+%C4%90%C3%AAm+Ch%C3%BA+Ki%E1%BB%87t+5+Garden/@10.8527677,106.7858182,766m/data=!3m2!1e3!4b1!4m6!3m5!1s0x317527ddc41d84d7:0x80774fd6502669a1!8m2!3d10.8527677!4d106.7883985!16s%2Fg%2F11zjltvzzz?entry=ttu&g_ep=EgoyMDI2MDYxMC4wIKXMDSoASAFQAw%3D%3D',
